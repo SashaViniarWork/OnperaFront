e911c3317cac4211b30f4a5b202e3bc429c18ea7
alexander.viniar@etiya.com <alexander.viniar@etiya.com>
2019-01-30 14:19:52 +0200

    information for html

--- src/app/component/landing/main/main.component.ts
+++ src/app/component/landing/main/main.component.ts
@@ -6,13 +6,49 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  images: any[];
+
+  slider: any[];
 
   constructor() {
-    this.images = [
-      {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fronalpstock_big.jpg/1200px-Fronalpstock_big.jpg'},
-      {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fronalpstock_big.jpg/1200px-Fronalpstock_big.jpg'},
-      {url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fronalpstock_big.jpg/1200px-Fronalpstock_big.jpg'}
+    this.slider = [
+      {
+        isDataSlide: true,
+        imgUrl: 'https://source.unsplash.com/LAaSoL0LrYs/1920x1080',
+        headerText: 'About Company',
+        index: '0',
+        text: 'Onpera Technologies is a professional services company that offers a rich pallet of solutions designed for increased\n' +
+          '          business productivity and efficiency. We developed our solutions according to your business needs and with the\n' +
+          '          end-users in mind. Our services portfolio is not what makes us different. What makes us different is who we are\n' +
+          '          and how we provide the services to our clients.'
+      },
+      {
+        isDataSlide: false,
+        index: '1',
+        imgUrl: 'https://source.unsplash.com/bF2vsubyHcQ/1920x1080',
+        headerText: 'Our Services',
+        text: 'Starting as early as you have an idea, we work with you to analyze, build, design, and release a final product to market.\n' +
+          '          We’re focused on understanding your users’ needs, validating hypotheses quickly, reducing risks, and continually improving\n' +
+          '          our processes to ensure highly efficient and fluid product development life cycle.'
+      },
+      {
+        isDataSlide: false,
+        index: '2',
+        imgUrl: 'https://source.unsplash.com/szFUQoyvrxM/1920x1080',
+        headerText: 'Our Teams',
+        text: ' <ul>\n' +
+          '          <li>Java Developers</li>\n' +
+          '          <li>Front-End Team</li>\n' +
+          '          <li>QA Engineers</li>\n' +
+          '        </ul>'
+      },
+      {
+        isDataSlide: false,
+        index: '3',
+        imgUrl: 'https://source.unsplash.com/szFUQoyvrxM/1920x1080',
+        headerText: 'Contact Us',
+        text: 'Contact Us'
+      },
+
     ];
   }
 
